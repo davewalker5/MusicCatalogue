c34ded83873782dfcc07fa99ea05cdd57de2b891
Dave Walker <david.walker5@icloud.com>
2023-10-03 18:30:03 +0100

    Add endpoint to retrieve artists

---
+++ src/MusicCatalogue.Api/Controllers/ArtistsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MusicCatalogue.Entities.Database;
+using MusicCatalogue.Logic.Factory;
+
+namespace MusicCatalogue.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    [Route("[controller]")]
+    public class ArtistsController : Controller
+    {
+        private readonly MusicCatalogueFactory _factory;
+
+        public ArtistsController(MusicCatalogueFactory factory)
+        {
+            _factory = factory;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Artist>>> GetArtistsAsync()
+        {
+            List<Artist> artists = await _factory.Artists.ListAsync(x => true);
+
+            if (!artists.Any())
+            {
+                return NoContent();
+            }
+
+            return artists;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Artist>> GetArtistsAsync(int id)
+        {
+            var artist = await _factory.Artists.GetAsync(x => x.Id == id);
+
+            if (artist == null)
+            {
+                return NotFound();
+            }
+
+            return artist;
+        }
+    }
+}
