82d6a565f7ef663b9237f571c54cad5e0b3b2af5
Dave Walker <david.walker5@icloud.com>
2023-10-31 22:08:44 +0000

    Added CSS menu bar with dropdowns

--- src/music-catalogue-ui/components/menuBar.js
+++ src/music-catalogue-ui/components/menuBar.js
@@ -1,43 +1,39 @@
 import styles from "./menuBar.module.css";
 import pages from "../helpers/navigation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 const MenuBar = ({ navigate, logout }) => {
   return (
-    <div className={styles.titleBarContainer}>
-      <div className={styles.titleElement}>
-        <img
-          src="./logo.png"
-          alt="Music Catalogue"
-          className={styles.logo}
-          onClick={() => navigate(pages.artists, null, null)}
-        />
+    <>
+      <div className={styles.navbar}>
+        <div className={styles.titleElement}>
+          <img
+            src="./logo.png"
+            alt="Music Catalogue"
+            className={styles.logo}
+            onClick={() => navigate(pages.artists, null, null)}
+          />
+        </div>
+        <div className={styles.title}>Music Catalogue</div>
+        <a onClick={() => logout()}>Log Out</a>
+        <div className={styles.dropdown}>
+          <button className={styles.dropbtn}>
+            Reports
+            <div className={styles.dropdownArrowContainer}>
+              <FontAwesomeIcon icon={faCaretDown} />
+            </div>
+          </button>
+          <div className={styles.dropdownContent}>
+            <a href="#">Job Status</a>
+          </div>
+        </div>
+        <a onClick={() => navigate(pages.export, null, null)}>Export</a>
+        <a href="#">Import</a>
+        <a onClick={() => navigate(pages.lookup, null, null)}>Search</a>
+        <a onClick={() => navigate(pages.artists, null, null)}>Artists</a>
       </div>
-      <div className={styles.title}>Music Catalogue</div>
-      <div className={styles.menuContainer}>
-        <span className={styles.menuItem} onClick={() => logout()}>
-          Log Out
-        </span>
-        <span
-          className={styles.menuItem}
-          onClick={() => navigate(pages.export, null, null)}
-        >
-          Export
-        </span>
-        <span className={styles.menuItem}>Import</span>
-        <span
-          className={styles.menuItem}
-          onClick={() => navigate(pages.lookup, null, null)}
-        >
-          Search
-        </span>
-        <span
-          className={styles.menuItem}
-          onClick={() => navigate(pages.artists, null, null)}
-        >
-          Artists
-        </span>
-      </div>
-    </div>
+    </>
   );
 };
 
