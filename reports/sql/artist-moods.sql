d4ee0850611ed1fe68b0f9bb4198234bfb934498
Dave Walker <david.walker5@icloud.com>
2026-01-31 16:32:09 +0000

    Populate the mood ToD weightings

--- reports/sql/artist-moods.sql
+++ reports/sql/artist-moods.sql
@@ -1,5 +1,9 @@
 SELECT  a.Id AS "Artist_Id",
-        m.Name AS "Mood_Name"
+        m.Name AS "Mood_Name",
+        m.MorningWeight,
+        m.AfternoonWeight,
+        m.EveningWeight,
+        m.LateWeight
 FROM    ARTISTS a
 JOIN    ARTIST_MOODS am ON am.ArtistId = a.Id
 JOIN    MOODS m ON m.Id = am.MoodId;
